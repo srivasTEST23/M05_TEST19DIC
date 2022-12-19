d79eed496b5820cf342ea2d7f916739cf4543522
Santi Rivas <ausias@h4-0.xiam.cat>
2022-12-19 12:25:49 +0100

    Merge origin/master
    Conflicts:
    	SegonIntent/src/segonintent/NewMain.java

--- SegonIntent/src/segonintent/NewMain.java
+++ SegonIntent/src/segonintent/NewMain.java
@@ -23,6 +23,12 @@ public class NewMain {
         int i =0;
         while (i<10)
             System.out.println("I vale: "+i);
+        
+
+        for (int j=0; j<10;j++) {
+            System.out.println("J vale: " + j);
+        }
+            
 
     }
     
